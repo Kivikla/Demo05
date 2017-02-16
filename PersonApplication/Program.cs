04228785e121e26f9479838f053c0c569c378eca
K2557 <K2557@OHJE-330-WS-08>
2017-02-16 08:14:47 +0200

    Demo05

--- PersonApplication/Program.cs
+++ PersonApplication/Program.cs
@@ -10,6 +10,58 @@ namespace PersonApplication
     {
         static void Main(string[] args)
         {
+            // create Persons (collecion)
+            Persons myFriends = new Persons();
+
+            // create a few Person objects
+            Person person1 = new Person
+            {
+                Firstname = "Kirsi",
+                Lastname = "Mainio",
+                SocialSecurityNumber = "123123-123"
+            };
+            Person person2 = new Person
+            {
+                Firstname = "Matti",
+                Lastname = "Huuso",
+                SocialSecurityNumber = "321321-321"
+            };
+            Person person3 = new Person
+            {
+                Firstname = "Teppo",
+                Lastname = "Taistelija",
+                SocialSecurityNumber = "121212-121"
+            };
+
+            // add Person objecs to Persons (myFriends)
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // get one person
+            Person person4 = myFriends.GetPerson(10);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("There is no person at that position!");
+            }
+
+            // print collction
+            myFriends.PrintData();
+
+            // find person
+            string SocialSecurityNumber = "123123-123";
+            Console.WriteLine("Find person with Socialsec.. :" + SocialSecurityNumber);
+            Person person5 = myFriends.FindPerson(SocialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            } else
+            {
+                Console.WriteLine("Can't find person with that Socialsec..");
+            }
         }
     }
 }
